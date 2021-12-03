7ea19f4649dc8e281ea86ae2cfc504cb2bda6881
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2021-12-03 10:49:08 +0100

    Add simple_download unittests

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -295,6 +295,30 @@ def symlink_callback(request, uri, response_headers):
     return [status_code, response_headers, json.dumps(ret)]
 
 
+def simple_download_callback(request, uri, response_headers):
+    if request.headers["Authorization"] != "Bearer VALID_TOKEN":
+        return [401, response_headers, '{"message": "Bad token; invalid JSON"}']
+
+    if request.headers["X-Machine-Name"] != "cluster1":
+        response_headers["X-Machine-Does-Not-Exist"] = "Machine does not exist"
+        return [
+            400,
+            response_headers,
+            '{"description": "Error on download operation", "error": "Machine does not exist"}',
+        ]
+
+    source_path = request.querystring.get("sourcePath", [None])[0]
+    if source_path == "/path/to/remote/source":
+        ret = "Hello!\n"
+        status_code = 200
+        return [status_code, response_headers, ret]
+    else:
+        response_headers["X-Invalid-Path"] = "path is an invalid path"
+        ret = {"description": "Error on download operation"}
+        status_code = 400
+        return [status_code, response_headers, json.dumps(ret)]
+
+
 httpretty.register_uri(
     httpretty.GET, "http://firecrest.cscs.ch/utilities/ls", body=ls_callback
 )
@@ -325,6 +349,10 @@ httpretty.register_uri(
 
 httpretty.register_uri(
     httpretty.POST, "http://firecrest.cscs.ch/utilities/symlink", body=symlink_callback
+)
+
+httpretty.register_uri(
+    httpretty.GET, "http://firecrest.cscs.ch/utilities/download", body=simple_download_callback
 )
 
 
@@ -580,3 +608,29 @@ def test_symlink_invalid_machine(valid_client):
 def test_symlink_invalid_client(invalid_client):
     with pytest.raises(firecrest.UnauthorizedException):
         invalid_client.symlink("cluster1", "/path/to/file", "/path/to/link")
+
+
+def test_simple_download(valid_client, tmp_path):
+    tmp_dir = tmp_path / "download_dir"
+    tmp_dir.mkdir()
+    local_file = tmp_dir / "hello.txt"
+    # Make sure this doesn't raise an error
+    valid_client.simple_download("cluster1", "/path/to/remote/source", local_file)
+
+    with open(local_file) as f:
+        assert f.read() == "Hello!\n"
+
+
+def test_symlink_invalid_arguments(valid_client):
+    with pytest.raises(firecrest.HeaderException):
+        valid_client.symlink("cluster1", "/path/to/invalid/file", "/path/to/local/destination")
+
+
+def test_symlink_invalid_machine(valid_client):
+    with pytest.raises(firecrest.HeaderException):
+        valid_client.symlink("cluster2", "/path/to/source", "/path/to/destination")
+
+
+def test_symlink_invalid_client(invalid_client):
+    with pytest.raises(firecrest.UnauthorizedException):
+        invalid_client.symlink("cluster1", "/path/to/source", "/path/to/destination")
