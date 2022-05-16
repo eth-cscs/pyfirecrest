be7f381afc27cab14bb8791e77f00bff23b9a917
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2022-05-16 11:03:48 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = '0.0.12'
+version = '1.0.0'
 
 setuptools.setup(
     name='pyfirecrest',
