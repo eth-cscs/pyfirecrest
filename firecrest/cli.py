bc785102459883354521810629840d70cf173760
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2022-11-04 13:48:27 +0100

    Add submit command

--- firecrest/cli.py
+++ firecrest/cli.py
@@ -480,6 +480,25 @@ def upload(
         raise typer.Exit(code=1)
 
 
+@app.command(rich_help_panel="Compute commands")
+def submit(
+    machine: str = typer.Argument(
+        ..., help="The machine name where the source filesystem belongs to."
+    ),
+    job_script: str = typer.Argument(..., help="The path of the script (if it's local it can be relative path, if it is on the machine it has to be the absolute path)"),
+    local: Optional[bool] = typer.Option(
+        True, help="The batch file can be local (default) or on the machine's filesystem."
+    ),
+):
+    """Submits a batch script to the workload manger of the target system
+    """
+    try:
+        console.print(client.submit(machine, job_script, local))
+    except fc.FirecrestException as e:
+        examine_exeption(e)
+        raise typer.Exit(code=1)
+
+
 @app.callback()
 def main(
     version: Optional[bool] = typer.Option(
