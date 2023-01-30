9bc3684b886402ffdd91c3b839b5d2d03d3da760
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2023-01-30 09:41:05 +0100

    Remove version file

--- firecrest/version.py
+++
@@ -1,9 +0,0 @@
-#
-#  Copyright (c) 2019-2022, ETH Zurich. All rights reserved.
-#
-#  Please, refer to the LICENSE file in the root directory.
-#  SPDX-License-Identifier: BSD-3-Clause
-#
-from . import __version__
-
-VERSION = __version__
