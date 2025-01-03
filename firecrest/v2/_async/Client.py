a6429a3fdc5ded917485123d7a68d72f2532a0b4
rsarm <rafael.sarmiento@cscs.ch>
2025-01-03 14:33:53 +0100

    Allow passing path to local script in client.submit (#142)
    * Run unittests for all PRs, not only the ones to main (#131)
    
    * submit with path to local file
    
    * fixes
    
    * fixes
    
    ---------
    
    Co-authored-by: Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
    Co-authored-by: Eirini Koutsaniti <ekoutsaniti@gmail.com>

--- firecrest/v2/_async/Client.py
+++ firecrest/v2/_async/Client.py
@@ -11,6 +11,7 @@ import asyncio
 import httpx
 import json
 import logging
+import os
 import pathlib
 import ssl
 
@@ -869,22 +870,42 @@ class AsyncFirecrest:
     async def submit(
         self,
         system_name: str,
-        script: str,
         working_dir: str,
+        script_str: Optional[str] = None,
+        script_path: Optional[str] = None,
         env_vars: Optional[dict[str, str]] = None,
     ) -> dict:
         """Submit a job.
 
         :param system_name: the system name where the filesystem belongs to
-        :param script: the job script
         :param working_dir: the working directory of the job
+        :param script_str: the job script
+        :param script_path: path to the job script
         :param env_vars: environment variables to be set before running the
                          job
         :calls: POST `/compute/{system_name}/jobs`
         """
+
+        if sum(arg is not None for arg in [script_str, script_path]) != 1:
+            self.log(
+                logging.ERROR,
+                "Exactly one of the arguments `script_str` or `script_path` "
+                "must be set."
+            )
+            raise ValueError(
+                "Exactly one of the arguments `script_str` or `script_path` "
+                "must be set."
+            )
+
+        if script_path:
+            if not os.path.isfile(script_path):
+                raise FileNotFoundError(f"Script file not found: {script_path}")
+            with open(script_path) as file:
+                script_str = file.read()
+
         data: dict[str, dict[str, Any]] = {
             "job": {
-                "script": script,
+                "script": script_str,
                 "working_directory": working_dir
             }
         }
